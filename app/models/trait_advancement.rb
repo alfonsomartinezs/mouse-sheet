bcd5ac035871ea731612cbe671762ce3ab9540ed
Alphaandsew <alfonso.martinez@shrikefound.com>
2020-08-17 11:46:56 -0700

    default level for trait advancement
    sets trait advancement level to 0 if one isn't chosen. prevents issues when increment trait with no level.

--- app/models/trait_advancement.rb
+++ app/models/trait_advancement.rb
@@ -1,4 +1,11 @@
 class TraitAdvancement < ApplicationRecord
   belongs_to :character
   belongs_to :trait
+
+  after_initialize :set_defaults, unless: :persisted? 
+
+  def set_defaults 
+    self.level ||= 0 
+  end
+
 end
